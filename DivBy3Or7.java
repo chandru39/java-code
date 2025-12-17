9b21f6c9be1262f6616f92af489d4bd18147e723
chandru39 <23becse039@kahedu.edu.in>
2025-12-17 10:45:40 +0530

    Add files via upload

---
+++ DivBy3Or7.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class DivBy3Or7{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n%3==0||n%7==0) System.out.println("Yes");
+else System.out.println("No");
+}
+}
